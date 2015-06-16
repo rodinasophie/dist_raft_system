40eff7d7dd3f30e422ee3954cd654399c83724d5
sonya <rodinasophie@gmail.com>
2015-06-16 12:41:42 +0300

    Graphs added

---
+++ cdf.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import sys
+import glob
+
+files=glob.glob("elections/*.txt")
+
+fig=plt.figure(facecolor='white')
+fig.suptitle('Election Test', fontsize=20, fontweight='bold')
+plt.xlabel('Microseconds', fontsize=18)
+plt.ylabel('Probability', fontsize=16)
+plt.locator_params(nbins=14)
+plt.xlim([0, 2500])
+plt.ylim([0, 1])
+plt.grid()
+#stri="Mean = " + str(int(np.mean(data)))
+#plt.figtext(0.6, 0.2,stri, fontweight='bold', fontsize='20',color='red')
+colors=['red','green', 'blue', 'yellow', 'orange', 'pink', 'grey','black', 'c']
+i=0
+for arg in files:
+    data = np.loadtxt(arg)
+    sorted_data = np.sort(data)
+    yvals=np.arange(len(sorted_data))/float(len(sorted_data))
+    plt.scatter(sorted_data,yvals,color=colors[i], s=0.7)
+    parts=arg.split('.')
+    parts=parts[0].split('/')
+    plt.plot(1,1,colors[i], label=parts[1])
+    i=i+1
+plt.legend( loc='upper left', numpoints = 1,prop={'size':12} )
+plt.show()
+plt.savefig("LeaderElection.png")
---
+++ out/scr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {1..10000}
+do
+	./client;
+done
